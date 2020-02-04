2b2d0f4a17167f408de4951be9cfe00ce23a6547
arsalandev <arsalankhan9286@gmail.com>
2020-02-05 03:08:49 +0500

    Apply Ag Grid with routing

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/counter'
+import DisplayData from './components/displaydata';
+import { Router, Route, Link, browserHistory } from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Counter hp="header" fp="footer" />, document.getElementById('root'));
+
+// ReactDOM.render((
+//     <Router >
+//        <Route path = "/" component = {Counter}>
+//           <Route path = "data" component = {DisplayData} />
+//        </Route>
+//     </Router>
+//  ), document.getElementById('root'));
+
+
+serviceWorker.register();
